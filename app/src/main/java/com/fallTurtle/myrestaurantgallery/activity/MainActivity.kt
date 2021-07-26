2da72ce7c1c2170f91a472911539dd4f491c7880
chmin1020 <chmin1020@gmail.com>
2021-07-26 21:48:16 +0900

    add intent and addActivity

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/MainActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/MainActivity.kt
@@ -4,13 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fallTurtle.myrestaurantgallery.R
+import com.fallTurtle.myrestaurantgallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding:ActivityMainBinding? = null
+    private val binding get()= mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivAddPic.setOnClickListener{
+            val addIntent = Intent(this, AddActivity::class.java)
+            startActivity(addIntent)
+        }
     }
 
-    val add_intent: Intent = Intent()
 
 }
