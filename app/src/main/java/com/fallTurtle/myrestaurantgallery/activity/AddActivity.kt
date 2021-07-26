ffe1381bba253ba7f6d6882b5b2ffb2ff6edcca5
chmin1020 <chmin1020@gmail.com>
2021-07-27 01:07:14 +0900

    finish

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
@@ -1,12 +1,26 @@
 package com.fallTurtle.myrestaurantgallery.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fallTurtle.myrestaurantgallery.R
+import com.fallTurtle.myrestaurantgallery.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
+    private var mBinding: ActivityAddBinding? = null
+    private val binding get()= mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivClear.setOnClickListener{ finish() }
+        binding.tvSave.setOnClickListener{
+            //저장~~
+            Toast.makeText(this,"저장되었습니다",Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
