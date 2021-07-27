1ad2f2bb021c2b70ae182d9dcc30db42db0b7629
chmin1020 <chmin1020@gmail.com>
2021-07-27 15:30:01 +0900

    link with firebase

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.fallTurtle.myrestaurantgallery.R
 import com.fallTurtle.myrestaurantgallery.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
