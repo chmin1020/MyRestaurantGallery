ff0be97a924152534fc95b2979fd7f2cd61dc56d
chmin1020 <chmin1020@gmail.com>
2021-07-29 13:40:57 +0900

    add picture function

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
@@ -1,10 +1,12 @@
 package com.fallTurtle.myrestaurantgallery.activity
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.fallTurtle.myrestaurantgallery.R
 import com.fallTurtle.myrestaurantgallery.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
@@ -17,10 +19,41 @@ class AddActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.ivClear.setOnClickListener{ finish() }
-        binding.tvSave.setOnClickListener{
+        binding.tvSave.setOnClickListener {
             //저장~~
-            Toast.makeText(this,"저장되었습니다",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "저장되었습니다", Toast.LENGTH_SHORT).show()
             finish()
+        }
+
+        binding.spGenre.adapter = ArrayAdapter.createFromResource(this, R.array.genre_spinner, android.R.layout.simple_spinner_dropdown_item)
+        binding.spGenre.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when(position){
+                    0 ->{
+                        binding.ivImage.setImageResource(R.drawable.korean_food)
+                    }
+                    1 ->{
+                        binding.ivImage.setImageResource(R.drawable.chinese_food)
+                    }
+                    2 ->{
+                        binding.ivImage.setImageResource(R.drawable.japanese_food)
+                    }
+                    3 ->{
+                        binding.ivImage.setImageResource(R.drawable.western_food)
+                    }
+                    4 ->{
+                        binding.ivImage.setImageResource(R.drawable.coffee_and_drink)
+                    }
+                    5 ->{
+                        binding.ivImage.setImageResource(R.drawable.drink)
+                    }
+                    6 ->{
+                        binding.ivImage.setImageResource(R.drawable.etc)
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
     }
 }
