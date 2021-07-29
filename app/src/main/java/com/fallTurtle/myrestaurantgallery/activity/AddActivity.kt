c14753111151e90e8fb6a2cddd114a430adb3410
chmin1020 <chmin1020@gmail.com>
2021-07-29 15:48:59 +0900

    gallery using is succeed, but store is still not succeed

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
@@ -1,17 +1,36 @@
 package com.fallTurtle.myrestaurantgallery.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.fallTurtle.myrestaurantgallery.R
 import com.fallTurtle.myrestaurantgallery.databinding.ActivityAddBinding
 
+
 class AddActivity : AppCompatActivity() {
     private var mBinding: ActivityAddBinding? = null
     private val binding get()= mBinding!!
+
+    //이미지를 갤러리에서 받아오기 위한 요소들
+    private var imgUri: Uri? = null
+    private var imgUsed = false
+    private val getImg = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result: ActivityResult ->
+        imgUri = result.data?.data
+        Log.d("uri!",imgUri.toString())
+
+        if(imgUri != null) {
+            binding.ivImage.setImageURI(imgUri)
+            imgUsed = true
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,34 +44,41 @@ class AddActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.ivImage.setOnClickListener{
+            val gallery =
+                Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            getImg.launch(gallery)
+        }
+
         binding.spGenre.adapter = ArrayAdapter.createFromResource(this, R.array.genre_spinner, android.R.layout.simple_spinner_dropdown_item)
         binding.spGenre.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                when(position){
-                    0 ->{
-                        binding.ivImage.setImageResource(R.drawable.korean_food)
-                    }
-                    1 ->{
-                        binding.ivImage.setImageResource(R.drawable.chinese_food)
-                    }
-                    2 ->{
-                        binding.ivImage.setImageResource(R.drawable.japanese_food)
-                    }
-                    3 ->{
-                        binding.ivImage.setImageResource(R.drawable.western_food)
-                    }
-                    4 ->{
-                        binding.ivImage.setImageResource(R.drawable.coffee_and_drink)
-                    }
-                    5 ->{
-                        binding.ivImage.setImageResource(R.drawable.drink)
-                    }
-                    6 ->{
-                        binding.ivImage.setImageResource(R.drawable.etc)
+                if(!imgUsed) {
+                    when (position) {
+                        0 -> {
+                            binding.ivImage.setImageResource(R.drawable.korean_food)
+                        }
+                        1 -> {
+                            binding.ivImage.setImageResource(R.drawable.chinese_food)
+                        }
+                        2 -> {
+                            binding.ivImage.setImageResource(R.drawable.japanese_food)
+                        }
+                        3 -> {
+                            binding.ivImage.setImageResource(R.drawable.western_food)
+                        }
+                        4 -> {
+                            binding.ivImage.setImageResource(R.drawable.coffee_and_drink)
+                        }
+                        5 -> {
+                            binding.ivImage.setImageResource(R.drawable.drink)
+                        }
+                        6 -> {
+                            binding.ivImage.setImageResource(R.drawable.etc)
+                        }
                     }
                 }
             }
-
             override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
     }
