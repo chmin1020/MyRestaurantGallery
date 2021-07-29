768b23f13a821b0aec8b9b602b8f53b8901d4c7e
chmin1020 <chmin1020@gmail.com>
2021-07-29 15:48:59 +0900

    gallery using is succeed, but store is still not succeed

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/item/Piece.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/item/Piece.kt
@@ -3,11 +3,20 @@ package com.fallTurtle.myrestaurantgallery.item
 import android.graphics.Bitmap
 
 class Piece{
+    private var imgUsed:Boolean = false
     private var image:Bitmap? = null
     private var name:String? = null
     private var location:String? = null
     private var genre:String? = null
     private var rate:Int? = null
+
+    fun setImgUsed(tf: Boolean){
+        imgUsed = tf
+    }
+
+    fun getImgUsed() :Boolean{
+        return imgUsed
+    }
 
     fun getImage() :Bitmap?{
         return image
