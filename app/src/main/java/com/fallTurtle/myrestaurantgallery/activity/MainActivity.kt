d541c836082fc6bd25e3387dba1f8d745232ed00
chmin1020 <chmin1020@gmail.com>
2021-07-31 23:46:22 +0900

    make logout, making withdrawal

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/MainActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/MainActivity.kt
@@ -3,6 +3,10 @@ package com.fallTurtle.myrestaurantgallery.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.GridLayoutManager
 import com.fallTurtle.myrestaurantgallery.R
 import com.fallTurtle.myrestaurantgallery.adapter.ListAdapter
@@ -30,9 +34,26 @@ class MainActivity : AppCompatActivity() {
             startActivity(addIntent)
         }
 
-        binding.ivMenu.setOnClickListener{
-            FirebaseAuth.getInstance().signOut();
-        }
+        binding.toolbar.setOnMenuItemClickListener(Toolbar.OnMenuItemClickListener { item: MenuItem ->
+            when(item.itemId){ //??????
+                R.id.menu_logout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    Toast.makeText(this,R.string.logout_success,Toast.LENGTH_SHORT).show()
+                    val login = Intent(this,LoginActivity::class.java)
+                    startActivity(login)
+                    finish()
+                    true
+                }
+                R.id.menu_withdrawal -> {
+                    FirebaseAuth.getInstance().currentUser!!.delete()
+                    Toast.makeText(this,R.string.withdrawal_success,Toast.LENGTH_SHORT).show()
+                    val login = Intent(this,LoginActivity::class.java)
+                    startActivity(login)
+                    true
+                }
+                else -> false
+            }
+        })
     }
 
 
