957216431664309b74650b0b1c48a82363e2adaa
chmin1020 <chmin1020@gmail.com>
2021-08-13 21:10:40 +0900

    loading ing

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/ProgressActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/ProgressActivity.kt
@@ -1,13 +1,21 @@
 package com.fallTurtle.myrestaurantgallery.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fallTurtle.myrestaurantgallery.databinding.ActivityProgressBinding
 import com.fallTurtle.myrestaurantgallery.item.ProgressDialog
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ProgressActivity: AppCompatActivity() {
     var mBinding : ActivityProgressBinding? = null
     val binding get() = mBinding!!
+
+    private fun toastMake(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +24,11 @@ class ProgressActivity: AppCompatActivity() {
 
         val pd = ProgressDialog(this)
         pd.create()
+
+        GlobalScope.launch {
+            delay(3000)
+            //toastMake("저장되었습니다.")
+            finish()
+        }
     }
 }
