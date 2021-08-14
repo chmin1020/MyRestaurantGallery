47b3e6d80e43393140416adb1695487247066d3f
chmin1020 <chmin1020@gmail.com>
2021-08-14 23:32:51 +0900

    add loading image on Glide

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/AddActivity.kt
@@ -183,7 +183,7 @@ class AddActivity : AppCompatActivity() {
                 //Toast.makeText(this, "저장되었습니다", Toast.LENGTH_SHORT).show()
                 val progress = Intent(this, ProgressActivity::class.java)
                 startActivity(progress)
-
+                finish()
             }
         }
     }
--- app/src/main/java/com/fallTurtle/myrestaurantgallery/etc/MyAppGlideModule.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/etc/MyAppGlideModule.kt
@@ -2,9 +2,12 @@ package com.fallTurtle.myrestaurantgallery.etc
 
 import android.content.Context
 import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+import com.fallTurtle.myrestaurantgallery.R
 import com.firebase.ui.storage.images.FirebaseImageLoader
 import com.google.firebase.storage.StorageReference
 import java.io.InputStream
@@ -17,6 +20,13 @@ class MyAppGlideModule : AppGlideModule() {
         registry.append(
             StorageReference::class.java, InputStream::class.java,
             FirebaseImageLoader.Factory()
+        )
+    }
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        builder.setDefaultRequestOptions(
+            RequestOptions().placeholder(R.drawable.loading_food)
         )
     }
 
