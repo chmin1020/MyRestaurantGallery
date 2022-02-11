5bba3c7269d82c0c5197bda2a0d14c3e6502be05
chmin1020 <chmin1020@gmail.com>
2022-02-12 00:18:20 +0900

    mapping to seoul temporary

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/MapActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/MapActivity.kt
@@ -2,15 +2,34 @@ package com.fallTurtle.myrestaurantgallery.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fallTurtle.myrestaurantgallery.R
 import com.fallTurtle.myrestaurantgallery.databinding.ActivityMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    //map object
+    private lateinit var mMap: GoogleMap
+
     private lateinit var binding:ActivityMapBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
 
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        val seoul= LatLng(37.56, 126.97)
+        val position = CameraPosition.Builder().target(seoul).zoom(16f).build()
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(position))
     }
 }
