3afbf1d3e88d0d169fd78198f40d8ab1e72eb72a
chmin1020 <chmin1020@gmail.com>
2023-01-08 01:18:46 +0900

    sub complete to convert login progress to viewmodel's duty

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/LoginActivity.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/activity/LoginActivity.kt
@@ -11,9 +11,8 @@ import com.fallTurtle.myrestaurantgallery.databinding.ActivityLoginBinding
 import com.fallTurtle.myrestaurantgallery.view_model.FirebaseUserViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
 
 /**
  * 앱의 로그인 화면을 담당하는 액티비티.
@@ -25,13 +24,15 @@ class LoginActivity: AppCompatActivity() {
     private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
 
     //google Login
-    private val mAuth by lazy { FirebaseAuth.getInstance() }
     private val mClient by lazy { GoogleSignIn.getClient(this, googleSignInOptions) }
     private lateinit var googleSignInOptions: GoogleSignInOptions
 
     //뷰모델
     private val viewModelFactory by lazy{ ViewModelProvider.AndroidViewModelFactory(this.application) }
     private val userViewModel by lazy { ViewModelProvider(this, viewModelFactory)[FirebaseUserViewModel::class.java] }
+
+    //로그인 인터페이스
+    private val loginSuccessInterface: OnCompleteListener<AuthResult> = OnCompleteListener { tryToShowMain(true) }
 
 
     //--------------------------------------------
@@ -40,19 +41,10 @@ class LoginActivity: AppCompatActivity() {
 
     //로그인 시도를 하고 그 결과를 가져오는 launcher
     private val getSignLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-        //로그인 시도 활동의 결과로 받은 task
-        val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
-
-        try {
-            //받은 결과의 아이디 토큰을 통해 파이어베이스 인증 시도
-            val account = task.getResult(ApiException::class.java) ?: throw NullPointerException()
-            val idToken = account.idToken ?: throw NullPointerException()
-            firebaseAuthWithGoogle(idToken)
-        }
-        catch (e: NullPointerException) {
-            // null -> 정상적인 데이터를 받아오지 못함
-            Toast.makeText(this, "구글 계정 인증 실패", Toast.LENGTH_SHORT).show()
-        }
+        //결과로 로그인 시도
+        userViewModel.getTokenForLogin(it.data)?.let { token->
+            userViewModel.loginUser(token, loginSuccessInterface) } ?:
+            run{ Toast.makeText(this, "구글 계정 인증 실패", Toast.LENGTH_SHORT).show() }
     }
 
 
@@ -72,7 +64,7 @@ class LoginActivity: AppCompatActivity() {
         binding.signInButton.setOnClickListener{ signIn() }
 
         //if already login -> skip this activity
-        mAuth.currentUser?.let { showMain(false) }
+        tryToShowMain(false)
     }
 
 
@@ -86,25 +78,14 @@ class LoginActivity: AppCompatActivity() {
         getSignLauncher.launch(signInIntent)
     }
 
-    /* 받은 아이디 토큰을 통해서 파이어베이스 인증을 시도하는 함수 */
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        //Token 보내서 credential 받아오기
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-
-        //받은 credential 통해 인증 시도
-        mAuth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                //인증이 되었다면 유저 데이터 받을 수 있음 -> 유저 데이터를 가지고 메인 화면으로 이동
-                if (task.isSuccessful)
-                    mAuth.currentUser?.let { showMain(true) }
-            }
-    }
-
     /* 파이어베이스 인증에도 성공했다면, 메인 화면으로 이동할 수 있게 하는 함수 */
-    private fun showMain(newLogin: Boolean) {
-        userViewModel.updateUser()
-        val intent = Intent(this, MainActivity::class.java).also { it.putExtra("newLogin", newLogin) }
-        startActivity(intent)
-        finish()
+    private fun tryToShowMain(newLogin: Boolean) {
+        if(userViewModel.checkUser()) {
+            val intent = Intent(this, MainActivity::class.java)
+                .also { it.putExtra("newLogin", newLogin) }
+            startActivity(intent)
+            if(newLogin) Toast.makeText(this, "로그인 완료!", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
