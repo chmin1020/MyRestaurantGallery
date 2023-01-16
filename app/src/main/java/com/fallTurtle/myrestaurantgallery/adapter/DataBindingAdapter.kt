e1262beb342b0864facef0739500cd3d08e7df11
chmin1020 <chmin1020@gmail.com>
2023-01-16 22:45:33 +0900

    try to put spinner setting in databinding

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/adapter/DataBindingAdapter.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/adapter/DataBindingAdapter.kt
@@ -1,8 +1,6 @@
 package com.fallTurtle.myrestaurantgallery.adapter
 
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
+import android.widget.*
 import androidx.databinding.BindingAdapter
 import coil.api.load
 import com.fallTurtle.myrestaurantgallery.R
@@ -18,23 +16,37 @@ object DataBindingAdapter {
                 crossfade(true)
                 placeholder(R.drawable.loading_food)
             }
-        } ?:
-        run{
-            when (categoryNum) {
-                0 -> imageView.setImageResource(R.drawable.korean_food)
-                1 -> imageView.setImageResource(R.drawable.chinese_food)
-                2 -> imageView.setImageResource(R.drawable.japanese_food)
-                3 -> imageView.setImageResource(R.drawable.western_food)
-                4 -> imageView.setImageResource(R.drawable.coffee_and_drink)
-                5 -> imageView.setImageResource(R.drawable.drink)
-                6 -> imageView.setImageResource(R.drawable.etc)
-            }
-        }
+        } ?: setDefaultImage(imageView, categoryNum)
     }
 
     @BindingAdapter(value = ["intNumberForText"])
     @JvmStatic
     fun setTextWithInt(textView: TextView, number: Int){
         textView.text = number.toString()
+    }
+
+    //////////////////////////////////////////////////
+
+    @BindingAdapter("entries")
+    @JvmStatic
+    fun Spinner.setEntries(entries: Array<String>?) {
+            entries?.let{
+                val arrayAdapter = ArrayAdapter(context, R.layout.support_simple_spinner_dropdown_item, it)
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                adapter = arrayAdapter
+            }
+    }
+
+
+    private fun setDefaultImage(imageView: ImageView, categoryNum: Int){
+        when (categoryNum) {
+            0 -> imageView.setImageResource(R.drawable.korean_food)
+            1 -> imageView.setImageResource(R.drawable.chinese_food)
+            2 -> imageView.setImageResource(R.drawable.japanese_food)
+            3 -> imageView.setImageResource(R.drawable.western_food)
+            4 -> imageView.setImageResource(R.drawable.coffee_and_drink)
+            5 -> imageView.setImageResource(R.drawable.drink)
+            6 -> imageView.setImageResource(R.drawable.etc)
+        }
     }
 }
