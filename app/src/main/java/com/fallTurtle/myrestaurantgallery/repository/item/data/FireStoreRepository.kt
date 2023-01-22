530f351c0324d7ecb5431a1ea5efe491b7c8f233
chmin1020 <chmin1020@gmail.com>
2023-01-22 17:57:40 +0900

    code comments work

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/repository/item/data/FireStoreRepository.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/repository/item/data/FireStoreRepository.kt
@@ -7,9 +7,18 @@ import com.google.firebase.firestore.QuerySnapshot
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+/**
+ * FireStore API 기능을 통해 아이템 처리를 수행하는 리포지토리.
+ **/
 class FireStoreRepository: DataRepository {
+    //파이어스토어에서 적절한 수행을 하기 위한 레퍼런스
     private fun collectionRef(): CollectionReference = FirebaseUtils.getStoreRef().collection("restaurants")
 
+
+    //--------------------------------------------
+    // 오버라이딩 영역
+
+    /* 모든 데이터를 가져오는 함수 */
     override suspend fun getAllData(): List<Info> {
         return suspendCoroutine { continuation ->
             collectionRef().get().addOnCompleteListener {
@@ -19,6 +28,7 @@ class FireStoreRepository: DataRepository {
         }
     }
 
+    /* 특정 데이터를 가져오는 함수 */
     override suspend fun getProperData(id: String): Info {
         return suspendCoroutine { continuation ->
             collectionRef().document(id).get().addOnCompleteListener {
@@ -28,16 +38,19 @@ class FireStoreRepository: DataRepository {
         }
     }
 
+    /* 데이터를 모두 지우는 함수 */
     override suspend fun clearData() {
         getAllData().forEach { deleteData(it) }
     }
 
+    /* 특정 데이터를 추가하는 함수 */
     override suspend fun insertData(data: Info) {
         suspendCoroutine<Any?> { continuation ->
             collectionRef().document(data.dbID).set(data).addOnCompleteListener { continuation.resume(null) }
         }
     }
 
+    /* 특정 데이터를 제거하는 함수 */
     override suspend fun deleteData(data: Info) {
         suspendCoroutine<Any?> { continuation ->
             collectionRef().document(data.dbID).delete().addOnCompleteListener { continuation.resume(null) }
--- app/src/main/java/com/fallTurtle/myrestaurantgallery/repository/item/image/StorageRepository.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/repository/item/image/StorageRepository.kt
@@ -5,14 +5,24 @@ import com.fallTurtle.myrestaurantgallery.model.firebase.FirebaseUtils
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+/**
+ * Firebase Storage API 기능을 통해 이미지 처리를 수행하는 리포지토리.
+ **/
 class StorageRepository: ImageRepository{
+    //파이어베이스 스토리지에서 적절한 수행을 하기 위한 레퍼런스
     private val storageRef = FirebaseUtils.getStorageRef()
 
+
+    //--------------------------------------------
+    // 오버라이딩 영역
+
+    /* 모든 이미지를 제거하는 함수 */
     override suspend fun clearImages() {
         val images = getAllImages()
         images.forEach { deleteImage(it) }
     }
 
+    /* 특정 이미지를 추가하는 함수 */
     override suspend fun insertImage(imageName: String, uri: Uri):String?{
         suspendCoroutine<Any?> { continuation ->
             storageRef.child(imageName).putFile(uri).addOnCompleteListener{ continuation.resume(null)}
@@ -24,9 +34,9 @@ class StorageRepository: ImageRepository{
                     continuation.resume(it.result.toString())
             }
         }
-
     }
 
+    /* 특정 이미지를 제거하는 함수 */
     override suspend fun deleteImage(imageName: String){
         suspendCoroutine<Any?> { continuation ->
             storageRef.child(imageName).delete().addOnCompleteListener { continuation.resume(null) }
@@ -34,6 +44,10 @@ class StorageRepository: ImageRepository{
     }
 
 
+    //--------------------------------------------
+    // 내부 함수 영역
+
+    /* 저장된 모든 이미지의 이름을 가져오는 함수 */
     private suspend fun getAllImages(): List<String> {
         return suspendCoroutine { continuation ->
             val images = mutableListOf<String>()
