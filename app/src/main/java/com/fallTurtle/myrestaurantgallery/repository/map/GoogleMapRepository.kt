ac7517b3a6d7a4fb8573841985dd1d3be2f9868e
chmin1020 <chmin1020@gmail.com>
2023-01-30 17:12:15 +0900

    map API version update

--- app/src/main/java/com/fallTurtle/myrestaurantgallery/repository/map/GoogleMapRepository.kt
+++ app/src/main/java/com/fallTurtle/myrestaurantgallery/repository/map/GoogleMapRepository.kt
@@ -3,13 +3,15 @@ package com.fallTurtle.myrestaurantgallery.repository.map
 import android.Manifest
 import android.app.Application
 import android.content.pm.PackageManager
+import android.location.Address
 import android.location.Geocoder
 import android.location.Location
+import android.os.Build
 import androidx.core.app.ActivityCompat
 import com.fallTurtle.myrestaurantgallery.model.retrofit.value_object.LocationPair
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.gms.tasks.Task
 import kotlin.coroutines.resume
@@ -35,7 +37,7 @@ class GoogleMapRepository(application: Application): MapRepository {
     override suspend fun requestCurrentLocation(): LocationPair? {
         return suspendCoroutine { continuation->
             if (checkMapPermission()) {
-                val locationTask = locationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, CancellationTokenSource().token)
+                val locationTask = locationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, CancellationTokenSource().token)
 
                 locationTask.addOnCompleteListener { task: Task<Location> ->
                     if (task.isSuccessful)
@@ -48,22 +50,23 @@ class GoogleMapRepository(application: Application): MapRepository {
     }
 
     /* 좌표를 주소로 변환하는 함수 */
-    override fun requestCurrentAddress(location: LocationPair) : String{
-        val addressMaker = StringBuilder()
-
+    override suspend fun requestCurrentAddress(location: LocationPair) : String{
         //geocoder 객체를 통해 현재 위도와 경도로 주소 받아오기 시도
-        geocoder.getFromLocation(location.latitude, location.longitude, 10)?.let{
-            if(it.isEmpty())
-                addressMaker.append("주소 찾을 수 없음")
-            else{ //받은 주소가 성공적이라면, 받은 리스트를 String 형식으로 변환
-                val str = it[0].getAddressLine(0).split(" ")
-                addressMaker.append(str[1])
-                for(num in 2 until str.size)
-                    addressMaker.append(" ${str[num]}")
+        return if(Build.VERSION.SDK_INT >= 33) {
+            suspendCoroutine { continuation ->
+                geocoder.getFromLocation(location.latitude, location.longitude, 10) {
+                    continuation.resume(addressCompleteWork(it.firstOrNull()))
+                }
+            }
+        } else{
+            suspendCoroutine { continuation ->
+                //blocking 함수를 suspend 영역에서 써서 오류 발생
+                //추후 해결해야 할 사항
+                geocoder.getFromLocation(location.latitude, location.longitude, 10)?.let {
+                    continuation.resume(addressCompleteWork(it.firstOrNull()))
+                }
             }
         }
-
-        return addressMaker.toString()
     }
 
 
@@ -74,5 +77,19 @@ class GoogleMapRepository(application: Application): MapRepository {
     private fun checkMapPermission() : Boolean {
         return (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun addressCompleteWork(addressFragment: Address?): String {
+        val addressMaker = StringBuilder()
+
+        //받은 주소가 성공적이라면, 받은 주소 정보를 알맞은 String 형식으로 변환
+        addressFragment?.also {
+            val str = it.getAddressLine(0).split(" ")
+            addressMaker.append(str[1])
+            for (num in 2 until str.size)
+                addressMaker.append(" ${str[num]}")
+        } ?: run { addressMaker.append("주소 찾을 수 없음") }
+
+        return addressMaker.toString()
     }
 }
